aec3db23840bedae30110863a2815c313d2c56ff
Dave Teply <davehamdan@gmail.com>
2025-01-26 15:04:04 -0600

    refactor: fixing linting issue

--- packages/dto/src/activity-set.ts
+++ packages/dto/src/activity-set.ts
@@ -1,7 +1,7 @@
 import { ActivityAttributeSetDTO } from './activity-attribute-set.js';
 
 export interface ActivitySetDTO {
-  activitySlug: String;
+  activitySlug: string;
   setStart?: Date;
   setCompleted?: Date;
   attributeSets?: ActivityAttributeSetDTO[];

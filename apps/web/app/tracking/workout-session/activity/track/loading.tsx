f95662263deb77f694d238eff111ff32b9d8ff34
Dave Teply <davehamdan@gmail.com>
2025-01-30 09:04:27 -0600

    layout: adding stubbed layout files

---
+++ apps/web/app/tracking/workout-session/activity/track/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <div className="flex justify-center mt-10">
+      <p>Loading...</p>
+      <span className="loading loading-ring loading-lg"></span>
+    </div>
+  );
+}
